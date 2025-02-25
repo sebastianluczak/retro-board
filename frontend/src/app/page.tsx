d45cb762a0fe1c9c75c2eee169f209d778ec0d29
Sebastian Łuczak <sebastian.m.luczak@gmail.com>
2025-02-25 11:51:47 +0100

    Remove useless use preventing deployment.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Login from "@/components/login/login";
-import {use, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {socket} from "@/app/socket";
 import Room from "@/components/room/room";
 import TopBar from "@/components/topbar/top-bar";
